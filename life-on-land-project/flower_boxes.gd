e63db8b04359cc6d39aa47f4659381e609c8b607
Bazguy <88348569+Bazguy@users.noreply.github.com>
2025-12-17 11:24:38 +0000

    added music

--- life-on-land-project/flower_boxes.gd
+++ life-on-land-project/flower_boxes.gd
@@ -4,5 +4,5 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	rotate_x(0.1)
+	rotate_y(0.1 * get_process_delta_time())
 	pass
